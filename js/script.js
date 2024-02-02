6c6b9ad39d0e6173b8508289ef9df62c9df6245d
chaminjud <cmj9972@gmail.com>
2024-02-02 14:43:22 +0900

    last commit

--- js/script.js
+++ js/script.js
@@ -12,5 +12,23 @@ $(function(){
    }).mouseleave(function(){
       $(this).find('.category-subnav').css('display','none');
    });
+
+$('.listview').click(function(e){
+    e.preventDefault();// e의 기능 중지 -> a에 기능이 중지됌
+    const view = $(this).data("view");
+    $("#pdlist>div").removeClass();
+    $('.listview rect').removeClass('list-act-color').addClass('list-color');
+    if(view ==3 ){
+        $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+        $("#pdlist>div").addClass("col-md-4 mb-5");
+    }else if(view == 4){
+        $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+        $("#pdlist>div").addClass("col-md-3 mb-5");
+    }else if(view == 5){
+        $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+        $("#pdlist>div").addClass("col-md-55 mb-5");
+
+    }
+   });
    
 });  
